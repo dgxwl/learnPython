518bd44c0e2f8cd42fabbe02aadb5217026282a9
wlxie <123456>
2018-09-10 15:45:03 +0800

    start to write guess

--- guess.py
+++ guess.py
@@ -1,3 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import math
+
+def generate:
+    chs = []
+    letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+	flags = []
+	for x in range(26)
+	    flags.append(False)
+    n = 0
+	while n < 5:
+	    while flags[n]
+		    index = random.randint(0, 25)
+		chs.append(letters[index])
+		flags[index] = True
+	
+	return chs
+	
