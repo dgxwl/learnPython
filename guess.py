b0aea2cc14590d8d89141aa2d4b0c79c1364d2be
dgxwl <1029325214@qq.com>
2018-10-26 17:51:58 +0800

    add 猜字符

--- guess.py
+++ guess.py
@@ -1,20 +1,47 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import math
+import random
 
 def generate():
-    chs = []
-    letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-	flags = []
-	for x in range(26)
-	    flags.append(False)
-    n = 0
-	while n < 5:
-	    while flags[n]
-		    index = random.randint(0, 25)
-		chs.append(letters[index])
-		flags[index] = True
-	
-	return chs
+	chs = ''
+	letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+	while True:
+		l = letters[random.randint(0, len(letters)-1)]
+		if chs.find(l) != -1:
+			continue
+		chs += l
+		if len(chs) == 5:
+			return chs
+
+def check(answer, guess):
+	result = [0, 0]
+	for i in range(len(answer)):
+		for j in range(len(guess)):
+			if answer[i] == guess[j]:
+				result[0] += 1
+				if i == j:
+					result[1] += 1
+	return result
+
+print('猜字符游戏. 输入EXIT退出')
+answer = generate()
+print(answer) #cheat
+wrongCount = 0
+while True:
+	if wrongCount < 10:
+		guess = input('猜吧: ').upper()
+		if guess == 'EXIT':
+			print('再见')
+			break
+		result = check(answer, guess)
+		if result[1] == len(answer):
+			print('猜对了!总分: %d' % (100 - wrongCount * 10))
+			break
+		else:
+			wrongCount += 1
+			print('猜对了%d个字符, 其中位置对了%d个' % (result[0], result[1]))
+	else:
+		print('游戏结束, 得分: 0')
+		break
 	
